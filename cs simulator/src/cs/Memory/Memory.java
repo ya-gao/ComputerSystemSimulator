7917c3f20745318160ae70b165bed06338c8e6ea
unknown <zwang61@gwu.edu>
2019-09-26 21:15:41 -0400

    add initializememory method

--- cs simulator/src/cs/Memory/Memory.java
+++ cs simulator/src/cs/Memory/Memory.java
@@ -1,5 +1,9 @@
 package Memory;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Vector;
 
 public class Memory {
@@ -50,6 +54,30 @@ public class Memory {
         }
         else {
             memory.set(addr,Value);
+        }
+    }
+
+    public void initializeMemory(){
+        File file = new File("memory.txt");
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            int line = 5; //available memory address starts from 5
+            while ((tempString = reader.readLine()) != null) {
+                memory.set(line,tempString);
+                line++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
         }
     }
 
